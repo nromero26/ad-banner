d967075b1c016bd3b1b184f83557a1b63fb271d2
nromero26 <nathalieromero26@gmail.com>
2016-05-20 08:56:15 -0400

    Add files via upload

---
+++ app banner ad_edge.js
@@ -0,0 +1,65 @@
+
+(function(compId){var _=null,y=true,n=false,x10='break-word',e31='${phone}',x50='65px',x43='Layer_5',x64='81px',x60='257px',x67='478px',e28='${button_2}',e37='${Guy3}',x63='397px',e32='${t4}',i='none',x73='Layer_72',x3='6.0.0.400',lf='left',x68='Layer_1',x47='auto',rbr='border-bottom-right-radius',x24='rgba(255,255,255,1)',x62='Layer_6',e29='${Girl2}',e81='${lgt2}',e26='${GD}',x79='300px',x1='6.0.0',x78='720px',x2='5.0.0',e35='${Girl3}',x4='rgba(0,0,0,0)',e36='${tURRIBLEbACKGROUND}',x46='256px',x58='Layer_4',e25='${twenty4}',x75='262px',g='image',x54='73px',x77='1',rtl='border-top-left-radius',x30='@@0@@px @@1@@px',x='text',x69='57px',e41='${button_1}',m='rect',x44='0px',x65='254px',h='height',x59='67px',x76='lgt2',x56='119px',e34='${Untitled-1}',x55='1px',x53='Layer_3',o='opacity',x51='251px',e40='${Girl-Guy}',rbl='border-bottom-left-radius',x71='Layer_2_lady_wit_purse',x45='52px',x74='68px',x9='Arial, Helvetica, sans-serif',l='normal',x49='Layer_7',e39='${Guy2}',e42='${drtk2}',rtr='border-top-right-radius',w='width',x8='24',e33='${Girl1}',e27='${bg2}',e38='${Text}',tp='top';var g12='t4.png',g15='Girl-Guy.png',g16='Girl3.png',g80='lgt2.png',g20='phone.png',g21='button%201.png',g13='Guy3.png',g6='drtk2.png',g61='Layer%204.png',g18='Girl1.png',g17='Girl2.png',g5='bg2.png',g23='Untitled-1.png',g57='Layer%203.png',g52='Layer%207.png',g19='GD.png',g14='Guy2.png',g70='Layer%201.png',g11='twenty4.png',g22='button%202.png',g72='Layer%202%20lady%20wit%20purse.png',g48='Layer%205.png',g66='Layer%206.png';var s7="<p style=\"margin: 0px;\">​<span style=\"font-family: allan, sans-serif; color: rgb(0, 0, 0); font-size: 36px;\">GET OUT OF LINE</span></p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={'allan, sans-serif':'<script src=\"http://use.edgefonts.net/allan:n7:all.js\"></script>','acme, sans-serif':'<script src=\"http://use.edgefonts.net/acme:n4:all.js\"></script>'},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'bg2',t:g,r:['2px','0px','715px','305px','auto','auto'],f:[x4,im+g5,'0px','0px'],tf:[[],[],[],['1.03','1.03']]},{id:'drtk2',t:g,r:['0px','0px','720px','300px','auto','auto'],o:'0',f:[x4,im+g6,'0px','0px']},{id:'tURRIBLEbACKGROUND',symbolName:'tURRIBLEbACKGROUND',t:m,r:['0','0','720','300','auto','auto'],o:'0'},{id:'Text',t:x,r:['7px','-5px','558px','56px','auto','auto'],o:'1',text:s7,n:[x9,[x8,],"rgba(6,6,6,1.00)",l,i,"",x10,l]},{id:'twenty4',t:g,r:['95px','80px','307px','40px','auto','auto'],o:'0',f:[x4,im+g11,'0px','0px']},{id:'t4',t:g,r:['28px','72px','56px','56px','auto','auto'],f:[x4,im+g12,'0px','0px']},{id:'Guy3',t:g,r:['335px','45px','96px','371px','auto','auto'],f:[x4,im+g13,'0px','0px']},{id:'Guy2',t:g,r:['163px','62px','70px','354px','auto','auto'],f:[x4,im+g14,'0px','0px']},{id:'Girl-Guy',t:g,r:['418px','54px','172px','369px','auto','auto'],f:[x4,im+g15,'0px','0px']},{id:'Girl3',t:g,r:['223px','76px','96px','341px','auto','auto'],f:[x4,im+g16,'0px','0px']},{id:'Girl2',t:g,r:['95px','76px','82px','341px','auto','auto'],f:[x4,im+g17,'0px','0px']},{id:'Girl1',t:g,r:['593px','71px','115px','352px','auto','auto'],f:[x4,im+g18,'0px','0px']},{id:'GD',t:g,r:['13px','30px','82px','373px','auto','auto'],o:'1',f:[x4,im+g19,'0px','0px']},{id:'phone',t:g,r:['473px','9px','325px','352px','auto','auto'],f:[x4,im+g20,'0px','0px']},{id:'button_1',t:g,r:['14px','246px','130px','40px','auto','auto'],f:[x4,im+g21,'0px','0px']},{id:'button_2',t:g,r:['148px','246px','130px','40px','auto','auto'],f:[x4,im+g22,'0px','0px']},{id:'Untitled-1',t:g,r:['0px','174px','346px','75px','auto','auto'],f:[x4,im+g23,'0px','0px']}],style:{'${Stage}':{isStage:true,r:['null','null','720px','300px','auto','auto'],overflow:'hidden',f:[x24]}}},tt:{d:4188,a:y,data:[["eid193",lf,4000,0,"easeInQuint",e25,'95px','95px'],["eid122",lf,2136,864,"easeOutQuad",e26,'13px','607px'],["eid280","filter.blur",4188,0,"easeInQuint",e27,'0px','0px'],["eid166",o,2916,1000,"easeInQuint",e28,'0','1'],["eid96",lf,1096,0,"easeInQuint",e29,'95px','95px'],["eid275",rtr,4000,0,"easeInQuint",e27,[0,11],[0,11],{vt:x30}],["eid136",w,3145,855,"easeInCubic",e31,'325px','277px'],["eid315",tp,4188,0,"easeInQuint",e27,'0px','0px'],["eid195",tp,4000,0,"easeInQuint",e32,'72px','72px'],["eid84",lf,1000,1000,"easeInQuint",e33,'593px','590px'],["eid197",o,2431,1569,"easeInQuint",e34,'0','1'],["eid277",rbl,4000,0,"easeInQuint",e27,[0,11],[0,11],{vt:x30}],["eid313",w,4188,0,"easeInQuint",e27,'715px','715px'],["eid132",lf,3145,855,"easeInCubic",e31,'491px','473px'],["eid196",lf,4000,0,"easeInQuint",e32,'28px','28px'],["eid310",lf,4188,0,"easeInQuint",e27,'2px','2px'],["eid99",lf,704,0,"easeInQuint",e35,'223px','223px'],["eid164",lf,4000,0,"easeInQuint",e28,'148px','148px'],["eid274",o,0,0,"easeInQuint",e27,'0','0'],["eid316",o,2899,601,"easeInQuint",e27,'0','0.54'],["eid317",o,3500,416,"easeInQuint",e27,'0.54','0.82'],["eid318",o,3916,199,"easeInQuint",e27,'0.82','1'],["eid143",o,3187,0,"easeInQuint",e36,'0','0'],["eid123",tp,2136,864,"easeOutQuad",e26,'30px','28px'],["eid165",tp,4000,0,"easeInQuint",e28,'246px','246px'],["eid272",rbr,4000,0,"easeInQuint",e27,[0,11],[0,11],{vt:x30}],["eid101",tp,704,1046,"easeInQuint",e35,'76px','310px'],["eid314",h,4188,0,"easeInQuint",e27,'305px','305px'],["eid115",lf,1417,0,"easeInQuint",e37,'335px','335px'],["eid85",tp,1000,1000,"easeInQuint",e33,'71px','319px'],["eid131",tp,3145,855,"easeInCubic",e31,'474px','9px'],["eid245",o,0,0,"easeInQuint",e38,'1','1'],["eid244",o,2250,1000,"easeInQuint",e38,'1','0'],["eid112",lf,1186,0,"easeInQuint",e39,'163px','163px'],["eid114",tp,1186,350,"easeInQuint",e39,'62px','315px'],["eid111",tp,1000,536,"easeInQuint",e40,'54px','321px'],["eid98",tp,1096,978,"easeInQuint",e29,'76px','318px'],["eid189",o,2250,1750,"easeInQuint",e32,'0','1'],["eid162",lf,4000,0,"easeInQuint",e41,'14px','14px'],["eid110",lf,1000,536,"easeInQuint",e40,'418px','421px'],["eid160",o,0,0,"easeInQuint",e26,'1','1'],["eid161",o,2664,481,"easeOutQuad",e26,'1','0'],["eid135",h,3145,855,"easeInCubic",e31,'352px','300px'],["eid186",o,0,3365,"easeInQuint",e25,'0','1'],["eid194",tp,4000,0,"easeInQuint",e25,'80px','80px'],["eid271",rtl,4000,0,"easeInQuint",e27,[0,11],[0,11],{vt:x30}],["eid145",o,3187,0,"easeInQuint",e42,'0','0'],["eid171",o,2916,1000,"easeInQuint",e41,'0','1'],["eid163",tp,4000,0,"easeInQuint",e41,'246px','246px'],["eid117",tp,1417,542,"easeInQuint",e37,'45px','310px']]}},"Symbol_1":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x43,t:g,r:[x44,x44,x45,x46,x47,x47],f:[x4,im+g48,x44,x44]}],style:{'${symbolSelector}':{r:[_,_,x45,x46]}}},tt:{d:0,a:y,data:[]}},"Symbol_2":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x49,t:g,r:[x44,x44,x50,x51,x47,x47],f:[x4,im+g52,x44,x44]}],style:{'${symbolSelector}':{r:[_,_,x50,x51]}}},tt:{d:0,a:y,data:[]}},"Symbol_3":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x53,t:g,r:[x54,x55,x56,x46,x47,x47],f:[x4,im+g57,x44,x44]},{id:x58,t:g,r:[x44,x44,x59,x60,x47,x47],f:[x4,im+g61,x44,x44]},{id:x62,t:g,r:[x63,x55,x64,x65,x47,x47],f:[x4,im+g66,x44,x44]}],style:{'${symbolSelector}':{r:[_,_,x67,x60]}}},tt:{d:0,a:y,data:[]}},"Symbol_4":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x68,t:g,r:[x44,x44,x69,x60,x47,x47],f:[x4,im+g70,x44,x44]}],style:{'${symbolSelector}':{r:[_,_,x69,x60]}}},tt:{d:0,a:y,data:[]}},"Symbol_5":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x71,t:g,r:[x44,x44,x69,x60,x47,x47],f:[x4,im+g72,x44,x44]}],style:{'${symbolSelector}':{r:[_,_,x69,x60]}}},tt:{d:0,a:y,data:[]}},"Symbol_6":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x73,t:g,r:[x44,x44,x74,x75,x47,x47],f:[x4,im+g52,x44,x44]}],style:{'${symbolSelector}':{r:[_,_,x74,x75]}}},tt:{d:0,a:y,data:[]}},"tURRIBLEbACKGROUND":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:g,id:x76,o:x77,r:[x44,x44,x78,x79,x47,x47],f:[x4,im+g80,x44,x44]}],style:{'${symbolSelector}':{r:[_,_,x78,x79]}}},tt:{d:4000,a:y,data:[["eid125",o,0,984,"easeInQuint",e81,'1','0'],["eid126",o,984,984,"easeInQuint",e81,'0.000000','1'],["eid127",o,1968,984,"easeInQuint",e81,'1','0'],["eid128",o,2952,1048,"easeInQuint",e81,'0.000000','1']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("bannerad");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){});
+//Edge binding end
+Symbol.bindTimelineAction(compId,symbolName,"Default Timeline","play",function(sym,e){sym.getSymbol("tURRIBLEbACKGROUND").play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${button_1}","click",function(sym,e){window.open("https://itunes.apple.com/us/app/pinterest/id429047995?mt=8&v0=WWW-NAUS-ITSTOP100-FREEAPPS&l=en&ign-mpt=uo%3D4","_blank");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${button_2}","click",function(sym,e){window.open("https://play.google.com/store","_blank");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${t4}","click",function(sym,e){window.open("http://twenty4theapp.businesscatalyst.com/index.html","_blank");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_1'
+(function(symbolName){})("Symbol_1");
+//Edge symbol end:'Symbol_1'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_2'
+(function(symbolName){})("Symbol_2");
+//Edge symbol end:'Symbol_2'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_3'
+(function(symbolName){})("Symbol_3");
+//Edge symbol end:'Symbol_3'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_4'
+(function(symbolName){})("Symbol_4");
+//Edge symbol end:'Symbol_4'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_5'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${Layer_2_lady_wit_purse}","mouseover",function(sym,e){sym.$("Layer_3").hide();sym.$("Layer_3").hide();sym.$("Layer_5").hide();sym.$("Layer_4").hide();sym.$("Layer_7").hide();sym.$("Layer_6").hide();});
+//Edge binding end
+})("Symbol_5");
+//Edge symbol end:'Symbol_5'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_6'
+(function(symbolName){})("Symbol_6");
+//Edge symbol end:'Symbol_6'
+
+//=========================================================
+
+//Edge symbol: 'tURRIBLEbACKGROUND'
+(function(symbolName){Symbol.bindTimelineAction(compId,symbolName,"Default Timeline","update",function(sym,e){});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",4000,function(sym,e){sym.play(0);});
+//Edge binding end
+})("tURRIBLEbACKGROUND");
+//Edge symbol end:'tURRIBLEbACKGROUND'
+})})(AdobeEdge.$,AdobeEdge,"bannerad");
